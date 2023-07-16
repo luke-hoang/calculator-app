c2549eca97647b9477640e1d78792df5314699af
luke-hoang <lh7190123@gmail.com>
2023-07-16 19:56:15 +1000

    Read value when digit is clicked

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-// change color theme
-document.getElementsByName('theme')
-  .forEach(theme => {
-    theme.addEventListener("change", (event) => {
-      document.documentElement.setAttribute('color-theme', event.target.value);
-    });
+class Calculator {
+  constructor() {
+    this.operator;
+    this.operand1;
+    this.operand2;
+    this.entry = '';
+
+    this.theme     = document.getElementsByName('theme'); 
+    this.output    = document.getElementById('output');
+    this.digits    = Array.from(document.getElementsByClassName('digit'));
+    this.operators = document.getElementsByClassName('operator');
+  }
+}
+
+
+const calc = new Calculator;
+
+calc.digits.forEach(digit => {
+  digit.addEventListener("click", (event) => {
+    calc.entry += event.target.value;
+    calc.output.textContent = calc.entry;
   });
+});
+
+// change color theme
+calc.theme.forEach(theme => {
+  theme.addEventListener("change", (event) => {
+    document.documentElement.setAttribute('color-theme', event.target.value);
+  });
+});
